6a47f6c8b5463deabae9d6695f36d7ab86781df4
albertostizzoli <albertostizzoli@yahoo.it>
2025-10-21 09:56:10 +0200

    added logo of project

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -144,7 +144,7 @@ function Sidebar() {
                     onClick={toggleSidebar}
                     className="text-white text-lg px-4 py-2 flex items-center gap-2 font-semibold">
                     <i className="fa-solid fa-globe"></i>
-                     <span className="relative inline-block
+                    <span className="relative inline-block
                     after:content-[''] after:absolute after:left-0 after:bottom-0 
                     after:w-0 after:h-[2px] after:bg-current 
                     after:transition-all after:duration-300 
@@ -188,32 +188,15 @@ function Sidebar() {
 
                 {/* Link fittizi raggruppati */}
                 <div className="flex flex-wrap gap-4 mt-2">
-                  <Link to="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">
-                    Privacy Policy
-                  </Link>
-                  <Link to="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">
-                    Supporto
-                  </Link>
-                  <Link to="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">
-                    Contatti
-                  </Link>
 
-                  <Link to="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">
-                    Assistenza
-                  </Link>
-                  <Link to="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">
-                    Condizioni d'uso
-                  </Link>
-                  <Link to="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">
-                    Copyright
-                  </Link>
-
-                  <Link to="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">
-                    Accessibilità
-                  </Link>
-                  <Link to="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">
-                    Chi Siamo
-                  </Link>
+                  <a href="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">Privacy Policy</a>
+                  <a href="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">Supporto</a>
+                  <a href="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">Contatti</a>
+                  <a href="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">Assistenza</a>
+                  <a href="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">Condizioni d'uso</a>
+                  <a href="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">Copyright</a>
+                  <a href="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">Accessibilità</a>
+                  <a href="#" className="text-white text-sm opacity-80 hover:underline w-[calc(33%-1rem)]">Chi Siamo</a>
                 </div>
 
                 {/* Copyright */}
